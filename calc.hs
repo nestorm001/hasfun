00a1a7617a32f4caa8dde1ddd6a6700f9e8cf46f
nesto <nestorm1@163.com>
2019-02-14 17:06:44 +0800

    init

---
+++ calc.hs
@@ -0,0 +1,5 @@
+double x = x + x
+triple x = double x + x
+doubleAndPlus x y = double x + double y
+doubleSmaller x y = if x < y then double x else double y
+doubleAndAddAll x y z = double x + double y + double z
